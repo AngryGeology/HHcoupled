2ead186c4f75021a123e0d9af1131ae3b84bc97e
jimmy <j.boyd1@lancaster.ac.uk>
2022-10-21 12:31:14 +0100

    Working folder cleanup

--- synth_warmup.py
+++ synth_warmup.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  4 13:46:16 2022
-Basic hydrological model of hollin hill 
+Synthetic warm up hydrological model of a synthetic hill slope with similar 
+attributes to hollin hill. 
 @author: jimmy
 """
 import os
@@ -12,82 +13,73 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 from scipy.spatial import cKDTree
-sys.path.append('RSUTRA')
-sys.path.append('/home/jimmy/phd/resipy/src')
+
+# add custom modules 
+if 'RSUTRA' not in sys.path: 
+    sys.path.append('RSUTRA')
+linux_r_path = '/home/jimmy/phd/resipy/src'
+win_r_path = r'C:\Users\boydj1\Software\resipy\src' 
+if sys.platform == 'linux' and linux_r_path not in sys.path: 
+    sys.path.append(linux_r_path)
+if 'win' in sys.platform.lower() and win_r_path not in sys.path:
+    sys.path.append(win_r_path)
 from resipy import meshTools as mt
 from SUTRAhandler import handler, material 
-from petroFuncs import ssf_petro, wmf_petro, wmf_petro_sat, ssf_polyn
 
 # setup script variables and functions 
 secinday = 24*60*60 
 plt.close('all')
 
+exec_loc = '/home/jimmy/programs/SUTRA_JB/bin/sutra'
+if 'win' in sys.platform.lower():
+    exec_loc = r'C:/Users/boydj1/Software/SUTRA/bin/sutra.exe'
+    
+    
+#create directory structure 
+masterdir='SyntheticStudy'
+modeldir= os.path.join(masterdir,'Models')
+datadir=os.path.join(masterdir,'Data')
+warmupdir = os.path.join(modeldir,'WarmUp')
+for dname in [masterdir,modeldir,datadir,warmupdir]:
+    if not os.path.exists(dname):
+        os.mkdir(dname)
+
 # %% step 0, load in relevant files
 rainfall = pd.read_csv(os.path.join('Rainfall', 'COSMOS_2015-2016.csv'))
-
 rainfall = pd.concat([rainfall[::2]]*5).reset_index()
+rainfall.loc[:,'EFF_RAIN'] = 0 # assume 5 mm rainfall for warmup period 
 # read in topo data
-topo = pd.read_csv('/home/jimmy/phd/Hollin_Hill/Coupled/topoData/2016-01-08.csv')
+topo = pd.read_csv('topoData/2016-01-08.csv')
 
-rainfall.loc[:,'EFF_RAIN'] = 5 
+# fit a generic polyline to get a similar slope profile of the actual hill 
+tx = topo['y'].values # actually grid is located against the y axis, make this the local x coordinate in 2d 
+p = np.polyfit(tx,topo['z'].values,1) # fit elevation to along ground distance data 
+tz = np.polyval(p,tx) # fit the modelled topography 
 
-# LOAD IN EXTENT OF WMF/SSF (will be used to zone the mesh)
-poly_ssf = np.genfromtxt('interpretation/SSF_poly_v3.csv',delimiter=',')
-poly_ssf_ext = np.genfromtxt('interpretation/SSF_poly_ext.csv',delimiter=',')
-poly_dogger = np.genfromtxt('interpretation/Dogger_poly.csv',delimiter=',')
-
-maxx = np.max(topo['y'])
-minx = np.min(topo['y'])
 
 # %% step 1, setup/create the mesh 
 # create quad mesh
-moutput = mt.quadMesh(topo['y'], topo['z'], elemx=1, pad=0, fmd=10,zf=1.1,zgf=1.1)
+moutput = mt.quadMesh(tx, tz, elemx=1, pad=0, fmd=10,zf=1.1,zgf=1.1)
 mesh = moutput[0]  # ignore the other output from meshTools here
 numel = mesh.numel  # number of elements
 # say something about the number of elements
 print('Number of mesh elements is %i' % numel)
 meshx = mesh.df['X']  # mesh X locations
 meshz = mesh.df['Z']  # mesh Z locations
-path = mpath.Path(poly_ssf)  # get path of staithes extent 
-
-# id points which are SSF 
-inside = path.contains_points(np.c_[meshx, meshz])
-# create a zone, 1 for staithes and 2 for whitby (which is taken to represent RMF too)
 zone = np.ones(mesh.numel, dtype=int)+1 
+
+# make anything above the median elevation be our WMF analogy, anything below SSF 
+medianz = 75# np.median(tz)
+inside = meshz < medianz
+
 zone[inside] = 1 # assign zone 1 to staithes 
-
-# assign extended zone outside of main investigation to staithes too 
-path = mpath.Path(poly_ssf_ext)
-inside = path.contains_points(np.c_[meshx, meshz])
-zone[inside] = 1 
-
-# assign dogger formation, and anything right of x = 280 
-path = mpath.Path(poly_dogger)
-inside = path.contains_points(np.c_[meshx, meshz])
-zone[inside] = 3 
-inside = meshx > 280
-zone[inside] = 3 
-
-# assign RMF formation (anything underneath the stiathes) or left of x = -10 
-inside = meshx < -10 
-zone[inside] = 4 
-for x in np.unique(meshx):
-    idxx = meshx == x 
-    zc = zone[idxx] # zone column 
-    if 1 in zc and 2 in zc:
-        cz = meshz[idxx] # column z coordinate, find minimum of zone 1 z coord 
-        cz_min = np.min(cz[zc==1])
-        for elm_id in mesh.df['elm_id'][idxx]:
-            if zone[elm_id-1] == 2 and meshz[elm_id-1] < cz_min:
-                zone[elm_id-1] = 4
+zone[np.invert(inside)] = 2 # assign zone 2 to WMF 
 
 # now to map zones to their respective identification numbers by element 
 zone_id = np.arange(mesh.numel,dtype=int)
 
 ssf_idx = zone_id[zone==1] #zone ssf 
 wmf_idx = zone_id[zone==2] #zone wmf 
-dog_idx = zone_id[zone==3] #zone dog 
-rmf_idx = zone_id[zone==4] #zone rmf  
 
 # get the surface of the mesh
 xz = mesh.extractSurface(False, False)
@@ -99,32 +91,22 @@ minx = np.min(mesh.node[:,0]) # boundary conditions later on
 #%% step 2 create materials 
 # STAITHES SANDSTONE 
 SSF = material(Ksat=0.11,theta_res=0.06,theta_sat=0.38,
-               alpha=0.000325,vn=1.82,name='STAITHES')
-#parameters taken from coupled approach 
+               alpha=0.1317,vn=2.5,name='STAITHES')
 WMF = material(Ksat=0.013,theta_res=0.1,theta_sat=0.48,
-               alpha=0.126e-4/2,vn=1.44,name='WHITBY')
-DOG = material(Ksat=0.309,theta_res=0.008,theta_sat=0.215,
-               alpha=1.27e-4,vn=1.45,name='DOGGER')
-RMF = material(Ksat=0.013,theta_res=0.1,theta_sat=0.48,
-               alpha=0.126e-4,vn=1.44,name='REDCAR')
-
-SSF.setPetro(ssf_polyn)
-WMF.setPetro(wmf_petro)
-DOG.setPetro(ssf_petro)
-RMF.setPetro(wmf_petro_sat)
+               alpha=0.0126,vn=1.44,name='WHITBY')
 
 #%% step 3, setup handler 
 ## create handler
-h = handler(dname='HydroBestFit', ifac=1,tlength=secinday,iobs=100, 
+h = handler(dname=warmupdir, ifac=1,tlength=secinday,iobs=100, 
             flow = 'transient',
             transport = 'transient',
             sim_type='solute')
-h.maxIter = 300
-h.rpmax = 5e3  
-h.drainage = 1e-2
+h.maxIter = 500
+h.rpmax = 10e3  
+h.drainage = 5e-2
 h.clearDir()
 h.setMesh(mesh)
-h.setEXEC('/home/jimmy/programs/SUTRA_JB/bin/sutra')
+h.setEXEC(exec_loc)
 
 ## compute cell depths 
 depths, node_depths = h.getDepths()
@@ -132,8 +114,6 @@ depths, node_depths = h.getDepths()
 #%% step 4 add materials to handler 
 h.addMaterial(SSF,ssf_idx)
 h.addMaterial(WMF,wmf_idx)
-h.addMaterial(DOG,dog_idx)
-h.addMaterial(RMF,rmf_idx)
 
 #%% step 5, boundary conditions (note not all of these are active)
 # find surface nodes
@@ -146,43 +126,12 @@ general_node = np.array([],dtype=int)
 general_type = []
 pres_node = np.array([],dtype=int) 
 
-# find nodes on right side of mesh 
-b1902x = mesh.node[:,0][np.argmin(np.sqrt((mesh.node[:,0]-106.288)**2))]
-right_side_idx = (mesh.node[:,0] == maxx) # & (zone_node == 2)
-# right_side_idx = (mesh.node[:,0] == b1902x) & (zone_node == 2)
-right_side_node = mesh.node[right_side_idx]
-right_side_topo = max(right_side_node[:,2])
-right_side_wt = 82.4 
-rs_delta = right_side_topo - right_side_wt 
-right_side_node_sat = right_side_node[right_side_node[:,2]<(right_side_topo-rs_delta)]
-dist, right_node = tree.query(right_side_node_sat[:,[0,2]])
-# pres_node = np.append(pres_node, right_node + 1) 
-
-cutoff = 50
-#find nodes on left side of mesh, set as drainage boundary below 55 od 
-left_side_idx = (mesh.node[:,0] == minx) & (mesh.node[:,2] < cutoff)
+#find nodes on left side of mesh, set as drainage boundary 
+left_side_idx = (mesh.node[:,0] == minx)
 left_side_node = mesh.node[left_side_idx]
 dist, left_node = tree.query(left_side_node[:,[0,2]])
 general_node = np.append(general_node,left_node + 1) 
 general_type = general_type + ['seep']*len(left_node)
-
-#find nodes on left side of mesh, set as seepage boundary above 55 od 
-left_side_idx = (mesh.node[:,0] == minx) & (mesh.node[:,2] > cutoff)
-left_side_node = mesh.node[left_side_idx]
-dist, left_node = tree.query(left_side_node[:,[0,2]])
-general_node = np.append(general_node,left_node + 1) 
-general_type = general_type + ['seep']*len(left_node)
-
-# hold pressure at borehole 1901 
-b1901_wt = 60.0 
-b1901x = mesh.node[:,0][np.argmin(np.sqrt((mesh.node[:,0]-26.501)**2))]
-b1901_idx = (mesh.node[:,0] == b1901x) & (zone_node==1)
-b1901_node = mesh.node[b1901_idx]
-b1901_topo = max(b1901_node[:,2])
-b1901_delta = b1901_topo - b1901_wt 
-b1901_node_sat = b1901_node[b1901_node[:,2]<(b1901_topo-b1901_delta)]
-dist, b1901_node = tree.query(b1901_node_sat[:,[0,2]])
-# pres_node = np.append(pres_node, b1901_node+1)
 
 # find nodes at base of mesh 
 max_depth = max(cell_depths)+1
@@ -190,20 +139,6 @@ dist, base_node = tree.query(np.c_[xz[0], xz[1]-max_depth])
 # pres_node = np.append(pres_node,base_node+1)
 general_node = np.append(general_node, base_node+1)
 general_type = general_type + ['pres']*len(base_node)
-
-# make top nodes a seepage boundary? 
-# general_node = np.append(general_node,source_node) 
-# general_type = general_type + ['seep']*len(source_node)
-
-# find nodes at base of ssf 
-ssf_x = np.unique(mesh.node[:,0][zone_node==1])
-ssf_z = np.zeros_like(ssf_x)
-for i,x in enumerate(ssf_x):
-    idx = (mesh.node[:,0]==x) & (zone_node==1)
-    z = mesh.node[:,2][idx]
-    ssf_z[i] = np.min(z)
-dist, ssf_base_node = tree.query(np.c_[ssf_x, ssf_z])
-# pres_node = np.append(pres_node, ssf_base_node+1) 
 
 # find mid points for computing cross sectional area for infiltration 
 dx = np.zeros(len(xz[0]))
@@ -244,30 +179,15 @@ ax.plot(TimeStamp,infil)
 #%% step 7, setup initial conditions 
 pres = np.zeros(mesh.numnp)
 
-pres[np.isnan(pres)] = 0 
-
 # compute pressure below water table 
 wt_depth = 5 #min(node_depths[pres==0])
 bl_depth = node_depths - wt_depth 
 bl_idx = pres==0 
 pres[bl_idx] = (9810*bl_depth[bl_idx])#*10e3  
 
-# compute pressure on right hand side of mesh 
-right_side_bl= right_side_wt - mesh.node[right_node][:,2]
-right_pressure_val = (9810*right_side_bl)
-h.pressure = max(right_pressure_val)
+h.pressure = max(pres)
 
-# hold pressure at zero on boreholes 
-b1901_bl= b1901_wt - mesh.node[b1901_node][:,2]
-hole_pressure = (9810*b1901_bl)
-
-# hold pressure at max pressure at base of mesh  
-base_pressure = np.full(len(ssf_base_node), max(right_pressure_val))
-
-# pressure_vals = right_pressure_val 
-pressure_vals = np.append(right_pressure_val, base_pressure)
-# pressure_vals = np.append(right_pressure_val,hole_pressure)
-# pressure_vals = np.append(pressure_vals, base_pressure)
+pressure_vals = [] # empty array 
 
 temp = [0]*mesh.numnp 
 pres[np.isnan(pres)] = 0 
@@ -321,12 +241,12 @@ h.vlim = [pmin, pmax]
 h.plot1Dresults()
 h.plotMeshResults()
 
-#%% convert to resistivity 
-h.callPetro()
+# save mesh output 
+# mesh.flipYZ()
+mesh.vtk(os.path.join(masterdir,'mesh.vtk'))
 
-#%% get final run 
+#%% get init conditions for real run 
 nstep = h.resultNsteps
 step = pd.DataFrame(h.nodResult['step%i'%(nstep-1)])
 step['zone'] = zone_node 
-step.to_csv(os.path.join(h.dname,'frun.csv'),index=False)
-
+step.to_csv(os.path.join(h.dname,'warm.csv'),index=False)
